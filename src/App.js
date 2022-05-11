ae5a5ce543ea97c7d4e4ba9e52ad8ea27ceaaf82
Professor-promise <professorpromise1000@gmail.com>
2022-05-11 12:27:20 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './components/Home/Home';
 import About from './components/About/About';
 import Services from './components/Services/Services';
 import Portfolio from './components/Portfolio/Portfolio';
-import Experience from './components/Experience/Experience';
+// import Experience from './components/Experience/Experience';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 
